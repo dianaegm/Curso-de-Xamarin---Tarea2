a24d7bce984e3a104812739c13ab794b50d923b8
degm <dianagaitan70@gmail.com>
2018-12-13 10:03:31 -0600

    Se agregaron archivos
    Se agregaron archivos

---
+++ Proyecto/Proyecto/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+
+namespace Proyecto
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+
+        }
+
+        async void Login_Clicked(object sender, EventArgs args)         {              await Navigation.PushAsync(new MainPage());         } 
+    }
+}
